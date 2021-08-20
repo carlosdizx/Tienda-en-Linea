9571223d761729fbfab76ae0a50de44d53760f42
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-20 16:35:29 -0500

    Cargue de productos directamente desde el json, se agregaron las imagnes necesarias para quq este proyecto funcione

---
+++ productos.js
@@ -0,0 +1,14 @@
+const PRODUCTOS = [
+  { nombre: "Producto 1", precio: 5000, path: "assets/imagenes/img_1.png" },
+  { nombre: "Producto 2", precio: 5000, path: "assets/imagenes/img_2.png" },
+  { nombre: "Producto 3", precio: 5000, path: "assets/imagenes/img_3.png" },
+  { nombre: "Producto 4", precio: 5000, path: "assets/imagenes/img_4.png" },
+  { nombre: "Producto 5", precio: 5000, path: "assets/imagenes/img_5.png" },
+  { nombre: "Producto 6", precio: 5000, path: "assets/imagenes/img_6.png" },
+  { nombre: "Producto 7", precio: 5000, path: "assets/imagenes/img_7.png" },
+  { nombre: "Producto 8", precio: 5000, path: "assets/imagenes/img_8.png" },
+  { nombre: "Producto 9", precio: 5000, path: "assets/imagenes/img_9.png" },
+  { nombre: "Producto 10", precio: 5000, path: "assets/imagenes/img_10.png" },
+  { nombre: "Producto 11", precio: 5000, path: "assets/imagenes/img_11.png" },
+  { nombre: "Producto 12", precio: 5000, path: "assets/imagenes/img_12.png" },
+];
