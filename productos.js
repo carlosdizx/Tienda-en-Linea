a981244b55e6858081e53b912fa873f901f835a4
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-20 16:57:08 -0500

    Completado el listado de productos e implmentados en pantalla

--- productos.js
+++ productos.js
@@ -1,14 +1,33 @@
 const PRODUCTOS = [
-  { nombre: "Producto 1", precio: 5000, path: "assets/imagenes/img_1.png" },
-  { nombre: "Producto 2", precio: 5000, path: "assets/imagenes/img_2.png" },
-  { nombre: "Producto 3", precio: 5000, path: "assets/imagenes/img_3.png" },
-  { nombre: "Producto 4", precio: 5000, path: "assets/imagenes/img_4.png" },
-  { nombre: "Producto 5", precio: 5000, path: "assets/imagenes/img_5.png" },
-  { nombre: "Producto 6", precio: 5000, path: "assets/imagenes/img_6.png" },
-  { nombre: "Producto 7", precio: 5000, path: "assets/imagenes/img_7.png" },
-  { nombre: "Producto 8", precio: 5000, path: "assets/imagenes/img_8.png" },
-  { nombre: "Producto 9", precio: 5000, path: "assets/imagenes/img_9.png" },
-  { nombre: "Producto 10", precio: 5000, path: "assets/imagenes/img_10.png" },
-  { nombre: "Producto 11", precio: 5000, path: "assets/imagenes/img_11.png" },
-  { nombre: "Producto 12", precio: 5000, path: "assets/imagenes/img_12.png" },
+  { nombre: "Arroz Blanco", precio: 3000, path: "assets/imagenes/img_1.png" },
+  { nombre: "Azucar", precio: 3000, path: "assets/imagenes/img_2.png" },
+  {
+    nombre: "Sal del Himalaya",
+    precio: 5000,
+    path: "assets/imagenes/img_3.png",
+  },
+  { nombre: "Panela", precio: 1500, path: "assets/imagenes/img_4.png" },
+  { nombre: "Jabon Dove", precio: 3500, path: "assets/imagenes/img_5.png" },
+  { nombre: "Champu Gliss", precio: 10000, path: "assets/imagenes/img_6.png" },
+  { nombre: "Harina Pan", precio: 4000, path: "assets/imagenes/img_7.png" },
+  { nombre: "Tuqeñas Limon", precio: 1200, path: "assets/imagenes/img_8.png" },
+  { nombre: "Leche larga vida", precio: 3000, path: "assets/imagenes/img_9.png" },
+  { nombre: "Queso Casero", precio: 6500, path: "assets/imagenes/img_10.png" },
+  { nombre: "CUM_mis Colacteos", precio: 6000, path: "assets/imagenes/img_11.png" },
+  { nombre: "Queso crema", precio: 3000, path: "assets/imagenes/img_12.png" },
 ];
+
+for (let i = 0; i < PRODUCTOS.length; i++) {
+  const producto = PRODUCTOS[i];
+  document.write(`<div class="col-sm-6 card">
+                  <img src="${
+                    producto.path
+                  }" width="450" height="450" class="card-img-top" />
+                      <div class="card-footer">
+                        precio ${producto.precio}
+                      </div>
+                      <div class="card-body">
+                        <h4>${[producto.nombre]}</h4>
+                      </div>
+                  </div>`);
+}
