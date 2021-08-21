592fd34cadf1c77ed62cedc8d61b2f0489f5e096
carlosdizx <carlodiaz@umariana.edu.co>
2021-08-21 07:41:03 -0500

    Completado el listado de productos e implmentados en pantalla

--- productos.js
+++ productos.js
@@ -1,20 +1,66 @@
 const PRODUCTOS = [
-  { nombre: "Arroz Blanco", precio: 3000, path: "assets/imagenes/img_1.png" },
-  { nombre: "Azucar", precio: 3000, path: "assets/imagenes/img_2.png" },
   {
+    id: 1,
+    nombre: "Arroz Blanco",
+    precio: 3000,
+    path: "assets/imagenes/img_1.png",
+  },
+  { id: 2, nombre: "Azucar", precio: 3000, path: "assets/imagenes/img_2.png" },
+  {
+    id: 3,
     nombre: "Sal del Himalaya",
     precio: 5000,
     path: "assets/imagenes/img_3.png",
   },
-  { nombre: "Panela", precio: 1500, path: "assets/imagenes/img_4.png" },
-  { nombre: "Jabon Dove", precio: 3500, path: "assets/imagenes/img_5.png" },
-  { nombre: "Champu Gliss", precio: 10000, path: "assets/imagenes/img_6.png" },
-  { nombre: "Harina Pan", precio: 4000, path: "assets/imagenes/img_7.png" },
-  { nombre: "Tuqeñas Limon", precio: 1200, path: "assets/imagenes/img_8.png" },
-  { nombre: "Leche larga vida", precio: 3000, path: "assets/imagenes/img_9.png" },
-  { nombre: "Queso Casero", precio: 6500, path: "assets/imagenes/img_10.png" },
-  { nombre: "CUM_mis Colacteos", precio: 6000, path: "assets/imagenes/img_11.png" },
-  { nombre: "Queso crema", precio: 3000, path: "assets/imagenes/img_12.png" },
+  { id: 4, nombre: "Panela", precio: 1500, path: "assets/imagenes/img_4.png" },
+  {
+    id: 5,
+    nombre: "Jabon Dove",
+    precio: 3500,
+    path: "assets/imagenes/img_5.png",
+  },
+  {
+    id: 6,
+    nombre: "Champu Gliss",
+    precio: 10000,
+    path: "assets/imagenes/img_6.png",
+  },
+  {
+    id: 7,
+    nombre: "Harina Pan",
+    precio: 4000,
+    path: "assets/imagenes/img_7.png",
+  },
+  {
+    id: 8,
+    nombre: "Tuqeñas Limon",
+    precio: 1200,
+    path: "assets/imagenes/img_8.png",
+  },
+  {
+    id: 9,
+    nombre: "Leche larga vida",
+    precio: 3000,
+    path: "assets/imagenes/img_9.png",
+  },
+  {
+    id: 10,
+    nombre: "Queso Casero",
+    precio: 6500,
+    path: "assets/imagenes/img_10.png",
+  },
+  {
+    id: 11,
+    nombre: "CUM_mis Colacteos",
+    precio: 6000,
+    path: "assets/imagenes/img_11.png",
+  },
+  {
+    id: 11,
+    nombre: "Queso crema",
+    precio: 3000,
+    path: "assets/imagenes/img_12.png",
+  },
 ];
 
 for (let i = 0; i < PRODUCTOS.length; i++) {
@@ -29,5 +75,15 @@ for (let i = 0; i < PRODUCTOS.length; i++) {
                       <div class="card-body">
                         <h4>${[producto.nombre]}</h4>
                       </div>
+                      <button class="btn btn-primary" id="btn-${
+                        producto.id
+                      }">Agregar</button>
                   </div>`);
 }
+
+const agregar = () => {
+  for (let i = 0; i < PRODUCTOS.length; i++) {
+    const producto = PRODUCTOS[i];
+    document.getElementById("btn-" + producto.id);
+  }
+};
