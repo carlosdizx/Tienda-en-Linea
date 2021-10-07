20fc70d6189312ca158db74913392f999e40cbc0
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-10-07 16:20:44 -0500

    Agregadas las rutas de las imagens y eliminadas las que se tenian de manera local

--- servidor.js
+++ servidor.js
@@ -8,68 +8,73 @@ const PRODUCTOS = [
     id: 1,
     nombre: "Arroz Blanco",
     precio: 3000,
-    path: "./assets/imagenes/img_1.png",
+    path: "https://www.merkadomi.com/wp-content/uploads/2019/06/arroz-diana-3000.png",
   },
   {
     id: 2,
     nombre: "Azucar Alkosto",
     precio: 3200,
-    path: "./assets/imagenes/img_2.png",
+    path: "https://comfandivirtual.com.co/supermercados/69603-large_default/azucar-manuelita-refinada-bolsa-plas-1000gr.jpg",
   },
   {
     id: 3,
     nombre: "Sal del Himalaya",
     precio: 5000,
-    path: "assets/imagenes/img_3.png",
+    path: "https://m.media-amazon.com/images/I/81OUEMsLSUL._SX425_.jpg",
   },
-  { id: 4, nombre: "Panela", precio: 1500, path: "assets/imagenes/img_4.png" },
+  {
+    id: 4,
+    nombre: "Panela",
+    precio: 1500,
+    path: "https://s.cornershopapp.com/product-images/2992305.jpg?versionId=wnh1rNMynBD7WVm0FBpwKUSC9lzVjg2g",
+  },
   {
     id: 5,
     nombre: "Jabon Dove",
     precio: 3500,
-    path: "assets/imagenes/img_5.png",
+    path: "https://www.jdh.com.co/wp-content/uploads/2020/08/jabon-dove-coco-x-3-unidades-1-600x600.jpg",
   },
   {
     id: 6,
     nombre: "Champu Gliss",
     precio: 10000,
-    path: "assets/imagenes/img_6.png",
+    path: "https://phantom-telva.unidadeditorial.es/9d7740c5a7b879ca5924f081d2c4d60f/resize/640/assets/multimedia/imagenes/2020/02/25/15826274434346.jpg",
   },
   {
     id: 7,
     nombre: "Harina Pan",
     precio: 4000,
-    path: "assets/imagenes/img_7.png",
+    path: "https://venemprende.shop/wp-content/uploads/2021/05/22.png",
   },
   {
     id: 8,
     nombre: "Tuqeñas Limon",
     precio: 1200,
-    path: "assets/imagenes/img_8.png",
+    path: "https://xn--tuqueas-8za.com/wp-content/uploads/2018/07/limon-papas.png",
   },
   {
     id: 9,
     nombre: "Leche larga vida",
     precio: 3000,
-    path: "assets/imagenes/img_9.png",
+    path: "https://images.jumpseller.com/store/fanero/4792261/Leche_entera_Colacteos.jpg?1595982998",
   },
   {
     id: 10,
     nombre: "Queso Casero",
     precio: 6500,
-    path: "assets/imagenes/img_10.png",
+    path: "https://insumosyalimentos.com.co/391-medium_default/queso-casero-colac-x-1000gr.jpg",
   },
   {
     id: 11,
-    nombre: "CUM_mis Colacteos",
+    nombre: "Kumis Colacteos",
     precio: 6000,
-    path: "assets/imagenes/img_11.png",
+    path: "https://mercaldas.vteximg.com.br/arquivos/ids/186432-1300-1300/Kumis-colacteos-x210-g.jpg?v=637076285320770000",
   },
   {
     id: 12,
     nombre: "Queso crema",
     precio: 3700,
-    path: "assets/imagenes/img_12.png",
+    path: "https://jumbocolombiafood.vteximg.com.br/arquivos/ids/198588-1000-1000/7702434701746.jpg?v=636250315216970000",
   },
 ];
 
@@ -80,10 +85,9 @@ const servidor = (request, response) => {
     let textoProductos = "";
     PRODUCTOS.forEach((producto) => {
       textoProductos += `<div class="col-sm-6 card">
-                  <img src="${path.resolve(
-                    __dirname,
-                    "assets/imagenes/img.png"
-                  )}" width="450" height="450" class="card-img-top" />
+                  <img src="${
+                    producto.path
+                  }" width="450" height="450" class="card-img-top" />
                       <div class="card-footer">
                         precio ${producto.precio}
                       </div>
@@ -98,7 +102,6 @@ const servidor = (request, response) => {
     textoHTML = textoHTML.replace("{productos}", textoProductos);
     response.writeHead(200, { "Content-type": "text/html; utf-8" });
     response.write(textoHTML);
-    console.log(textoHTML);
     response.end();
   });
 };
